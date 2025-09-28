dbf2590d2e3abbe0c62a628c2e00ade2026f67d0
jozz <72740829+jozzzzep@users.noreply.github.com>
2025-09-28 19:12:45 +0300

    Delete box_not_matching.dart

--- lib/src/exceptions/box_not_matching.dart
+++
@@ -1,7 +0,0 @@
-class BoxNotMatchingException implements Exception {
-  final String message;
-  BoxNotMatchingException(this.message);
-
-  @override
-  String toString() => 'BoxNotMatchingException: $message';
-}
