95dd1f6607e83d8815081fb6a08c16bee9c8ad73
jozz <72740829+jozzzzep@users.noreply.github.com>
2025-10-08 23:49:17 +0300

    Added lock for all configured boxes

--- hivez/lib/src/special_boxes/configured/configured_box.dart
+++ hivez/lib/src/special_boxes/configured/configured_box.dart
@@ -1,14 +1,21 @@
 library;
 
+import 'package:meta/meta.dart' show internal;
+import 'package:synchronized/synchronized.dart' show Lock;
+
 import 'package:hivez/src/boxes/boxes.dart';
 import 'package:hivez/src/builders/builders.dart';
 
 part 'extensions.dart';
+part 'shared_lock.dart';
 
 class ConfiguredBox<K, T> extends BoxDecorator<K, T> {
   final BoxConfig config;
+  final SharedLock sharedLock;
 
   ConfiguredBox(
-    this.config,
-  ) : super(config.createBox<K, T>());
+    this.config, {
+    SharedLock? sharedLock,
+  })  : sharedLock = sharedLock ?? SharedLock(),
+        super(config.createBox<K, T>());
 }
