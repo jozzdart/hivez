c9e202cfa032b82c0919c514e8e0d3111d0d95f6
jozz <72740829+jozzzzep@users.noreply.github.com>
2025-10-09 17:33:14 +0300

    Update lock.dart

--- hivez/lib/src/special_boxes/indexed/lock.dart
+++ hivez/lib/src/special_boxes/indexed/lock.dart
@@ -6,40 +6,27 @@ class IndexedBoxLock extends SharedLock {
   IndexedBoxLock(this.indexedBox, {super.overrideLock});
 
   @override
+  Future<void> Function(String opName, Object error, StackTrace stack)?
+      get onError => _onError;
+
+  Future<void> _onError(String opName, Object error, StackTrace stack) async =>
+      indexedBox.logger?.call(_msg(opName, error, stack));
+
+  String _msg(String opName, Object error, StackTrace stack) {
+    return '[ERROR with Indexed Box: ${indexedBox.name}] [$opName] Unexpected error: $error\n$stack';
+  }
+}
+
+class IndexedBoxLockJournal extends IndexedBoxLock {
+  IndexedBoxLockJournal(super.indexedBox, {super.overrideLock});
+
+  @override
   Future<R> runner<R>(Future<R> Function() body) async {
     return await indexedBox._journal.runWrite(body);
   }
-
-  @override
-  Future<void> Function(String opName, Object error, StackTrace stack)?
-      get onError => _onError;
-
-  Future<void> _onError(String opName, Object error, StackTrace stack) async =>
-      indexedBox.logger?.call(_msg(opName, error, stack));
-
-  String _msg(String opName, Object error, StackTrace stack) {
-    return '[ERROR with Indexed Box: ${indexedBox.name}] [$opName] Unexpected error: $error\n$stack';
-  }
 }
 
-class IndexedBoxLockBypassJournal extends SharedLock {
-  final IndexedBox indexedBox;
-
-  IndexedBoxLockBypassJournal(this.indexedBox, {super.overrideLock});
-
-  @override
-  Future<void> Function(String opName, Object error, StackTrace stack)?
-      get onError => _onError;
-
-  Future<void> _onError(String opName, Object error, StackTrace stack) async =>
-      indexedBox.logger?.call(_msg(opName, error, stack));
-
-  String _msg(String opName, Object error, StackTrace stack) {
-    return '[ERROR with Indexed Box: ${indexedBox.name}] [$opName] Unexpected error: $error\n$stack';
-  }
-}
-
-extension IndexedBoxLockExtension on IndexedBoxLock {
-  IndexedBoxLockBypassJournal get bypassJournal =>
-      IndexedBoxLockBypassJournal(indexedBox, overrideLock: internalLock);
+extension IndexedBoxLockExtension on IndexedBoxLockJournal {
+  IndexedBoxLock get bypassJournal =>
+      IndexedBoxLock(indexedBox, overrideLock: internalLock);
 }
